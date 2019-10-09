a6c579e9313b319910ee95a82a77df99e1b97e15
zhengjunxiang <18810076541@163.com>
2019-10-09 16:00:37 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <com-color-button type="success" :disabled="false">按钮</com-color-button> 
+    <com-color-button :disabled="true">按钮</com-color-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ComColorButton from '../packages/index'
+// 注册组件库
+Vue.use(ComColorButton)
+
 Vue.config.productionTip = false
 
 new Vue({
